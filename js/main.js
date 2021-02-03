300faf98caa610c258349c9ab2a03ff3fcd084c1
Your NameNikolay <nikolya_usov@bk.ru>
2021-02-04 00:49:51 +0300

    Создал функциюю по генерации объекта

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 
 //Константы
+const TITLE_OF_HOTEL =
+['Аппарт дворец в Геленджике',
+  'Moscow Inn',
+  'SpaSun',
+  'Красный бермуд',
+  'Сочи-парк',
+  'Отель Прованс',
+  'Holiday inn',
+  'Marriot',
+  'Blue rooms'];
+
+const TYPE_OF_HOTEL = ['palace', 'flat', 'house', 'bungalow'];
+const TIME_OF_CHECKIN = ['12:00', '13:00','14:00'];
+const TIME_OF_CHECKOUT = ['12:00','13:00',' 14:00'];
+const FEATURES_OF_HOTEL = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTO_OF_HOTEL = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 // Получение селекторов
 // Обработчики событий
 // Вспомогательные Функции
@@ -36,31 +52,76 @@ const getRandomFloatInRange = (min, max, significand = 2) => {
 
 getRandomFloatInRange(1, 1, 2)
 
+// Функция выдающая случайный элемент из массива
+
+const getRandomArrElement = (arr) => arr[getRandomIntegerInRange(0, arr.length-1)];
+
+// Функция уникальных произвольных элементов из массива
+const getRandomUniqueArr = (arr) => {
+  let uniqueArr = [];
+  const uniqueLength = getRandomIntegerInRange(1, arr.length-1);
+  for(let i = 0; i < uniqueLength; i++) {
+    let elem = arr[getRandomIntegerInRange(0, arr.length-1)];
+    if(!uniqueArr.includes(elem)){
+      uniqueArr.push(elem);
+    }
+  }
+
+  return uniqueArr;
+}
+
+// Функция создающая массив случайной длинны
+const getRandomArr = (arr) => {
+  let randomLength = getRandomIntegerInRange(1, 10);
+  let newArr = [];
+  for(let i = 0; i < randomLength; i++) {
+    newArr.push(getRandomArrElement(arr))
+  }
+
+  return newArr
+}
+
+//Функция генератор предложения
+const createRandomDescription = (element='место') => {
+  const into = ['Это изумительное ','Это раскошное ','Это потрясающее ','Это бюджетное ',' Это спокойное ','Это элитное ','Это королевское'];
+  const features = ['c кондиционером',' с wi-fi','c слифтом','c парковкой','c душем'];
+  const tagline = ['доведут тебя к мечтам','исполнит твои желания','заставит забыть все невсзгоды','подарит любовь','создаст комфорт','наполнит отдых заботой','вознесет тебя до небес'];
+  let phrase = getRandomArrElement(into) + element+ ' ' + getRandomArrElement(features) +' '+ getRandomArrElement(tagline);
+
+  return phrase
+}
+
 //Функция создающая объявление( тип данных- объект)
-const createHotel = () => {
+
+function createHotel() {
+
   return {
     author: {
-      avatar: 'img/avatars/user{{xx}}.png',
+      avatar: 'img/avatars/user0' + getRandomIntegerInRange(0, 8) + '.png',
     },
     offer: {
-      title: '',
-      address: '',
-      price: '',
-      type: '',
-      rooms: '',
-      guests: '',
-      checkin: '',
-      checkout: '',
-      features: '',
-      description: '',
-      photos: '',
+      title: getRandomArrElement(TITLE_OF_HOTEL),
+      address: location.x,
+      price: getRandomIntegerInRange(0, 100000),
+      type: getRandomArrElement(TYPE_OF_HOTEL),
+      rooms: getRandomIntegerInRange(1, 10),
+      guests: getRandomIntegerInRange(1, 100),
+      checkin: getRandomArrElement(TIME_OF_CHECKIN),
+      checkout: getRandomArrElement(TIME_OF_CHECKOUT),
+      features: getRandomUniqueArr(FEATURES_OF_HOTEL),
+      description: createRandomDescription(),
+      photos: getRandomArr(PHOTO_OF_HOTEL),
     },
     location: {
-      x: '',
-      y: '',
+      x: getRandomFloatInRange(35.65000, 35.70000, 5),
+      y: getRandomFloatInRange(139.70000, 139.80000, 5),
     },
-  }
-};
+  };
+}
+
+// eslint-disable-next-line no-unused-vars
+const hostelsData = new Array(10).fill(null).map(() => createHotel());
+
 /*
 В файле main.js на основе написанных в прошлом задании утилитарных функций напишите необходимые функции для создания массива из 10 сгенерированных JS-объектов. Каждый объект массива — описание похожего объявления неподалёку.
 
