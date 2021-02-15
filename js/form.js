715492487b555d1fe2b729b9d80cb44657fe0d5c
Your NameNikolay <nikolya_usov@bk.ru>
2021-02-15 21:27:43 +0300

    feat: add campre input type and input price

--- js/form.js
+++ js/form.js
@@ -1,4 +1,20 @@
+const MIN_PRICE_OF_TYPE = {
+  palace: '10000',
+  flat: '1000',
+  house: '5000',
+  bungalow: '0',
+}
+
 const form = document.querySelector('.ad-form')
 const selectType = form.querySelector('#type');
-// eslint-disable-next-line no-console
-console.log(selectType);
+const inputPrice = form.querySelector('#price')
+
+const selectedOption  = selectType.options[selectType.selectedIndex];
+
+inputPrice.setAttribute('placeholder', MIN_PRICE_OF_TYPE[selectedOption.value]);
+inputPrice.setAttribute('min', MIN_PRICE_OF_TYPE[selectedOption.value]);
+
+selectType.addEventListener('change', function () {
+  inputPrice.setAttribute('placeholder', MIN_PRICE_OF_TYPE[this.value]);
+  inputPrice.setAttribute('min', MIN_PRICE_OF_TYPE[this.value]);
+});
