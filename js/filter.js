4ca72463d6486a6ce427d63b8fa076a87706e4ea
Your NameNikolay <nikolya_usov@bk.ru>
2021-02-28 13:40:33 +0300

    init: create filter function

--- js/filter.js
+++ js/filter.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+
 const filterForm = document.querySelector('.map__filters');
 const houeType = filterForm.querySelector('#housing-type');
 const housePrice = filterForm.querySelector('#housing-price');
@@ -16,6 +17,29 @@ const getCheckedCheckBoxesValue = () => {
   return checkedValue;
 }
 
+
+const compareFeatures = (filter, data) => {
+  if (filter.length === 0) {
+
+    return true;
+
+  } else if ( filter.length > data.length) {
+
+    return false;
+
+  } else {
+    filter.forEach((elem) => {
+      if(data.indexOf(elem) === -1) {
+
+        return false
+      }
+    })
+
+    return true
+  }
+
+}
+
 const filterHostel = (hostel) => {
   const {offer} = hostel;
   const {
@@ -26,9 +50,31 @@ const filterHostel = (hostel) => {
     features,
   } = offer;
 
-  if(price < housePrice.value) {
+  const activeFeatures = getCheckedCheckBoxesValue()
+  // описываем условие когда фильтрации нет
+  if(houeType.value === 'any' && housePrice.value === 'any' && houseRooms.value === 'any' && houseGuests.value === 'any' && activeFeatures.length === 0) {
+    return true
+  }
+  //если тип жилья не равен типу в фильрах возвращаем ложь
+  if (houeType.value !== type) {
     return false
   }
+  // цена фильтра должна быть мень цены хостела
+  /*   if(price < housePrice.value) {
+    eturn false
+  }; */
+
+  //  количествокомнат должно быть равно
+  if (rooms !== houseRooms.value) {
+    return false;
+  }
+  // кооличество гостей не должно быть больше чем в карточке товара
+  if (guests <= houseGuests.value) {
+    return false;
+  }
+
+  // сравнить выбранные чекбоксы и карточку
+  compareFeatures(activeFeatures, features)
 }
 
 filterForm.addEventListener('change' )
